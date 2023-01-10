4043295201ac5f6b3c9daf19238eda4802150a24
jinmichael_98 <jinmichael_98@live.com>
2023-01-09 19:58:29 -0600

    Remove comments

--- bmwam-embedded/src/main.cpp
+++ bmwam-embedded/src/main.cpp
@@ -9,18 +9,11 @@ void setup() {
         delay(10);
     
     sht30.init();
-    //   if (! sht30.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    //     Serial.println("Couldn't find SHT31");
-    //     while (1) delay(1);
-    //   }
-
-    // pinMode(16, OUTPUT);
 }
 
 void loop() {
     ultrasonic.printDistance();
     sht30.printTemp();
-    // digitalWrite(16, !digitalRead(16));
 
     Serial.println("-----");
     delay(1000);
