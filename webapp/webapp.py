0fb05f752c647f63b9316ace8a5b6e8192058c3b
jinmichael_98 <jinmichael_98@live.com>
2023-03-28 11:18:44 -0500

    Added mock timestamps

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,11 +1,14 @@
 from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 import requests
 
+from datetime import datetime, timedelta
+
 HEADER_MAP = {
-    'Distance': 'Distance(mm)',
+    'Distance': 'Distance(m)',
     'Temperature': 'Temperature(Celsius)',
     'Spectral Data': 'F1(405-425)nm'
 }
@@ -34,7 +37,7 @@ def update_data():
     return df
 
 
-def extract_json_from_notehub():
+def authenticate_notehub():
     auth_url = 'https://notehub.io/oauth2/token'
     auth_data = {
         'grant_type': 'client_credentials',
@@ -44,31 +47,56 @@ def extract_json_from_notehub():
 
     auth_response = requests.post(auth_url, data=auth_data)
 
-    event_url = 'https://api.notefile.net/v1/projects/app:2cd7bc69-cf7d-4599-9940-247d9fcf2f6c/events'
-    event_auth = auth_response.json()['token_type'] + ' ' + auth_response.json()['access_token']
+    return auth_response
+
+
+def extract_json_from_notehub():
+    event_auth = auth_token.json()['token_type'] + ' ' + auth_token.json()['access_token']
     event_headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': event_auth}
-    
+    event_url = 'https://api.notefile.net/v1/projects/app:2cd7bc69-cf7d-4599-9940-247d9fcf2f6c/events'
+
     event_response = requests.get(event_url, headers=event_headers)
 
     return event_response.json()
 
+
+
+# --- Start of program ---
+
+auth_token = authenticate_notehub()
 df = update_data()
+df['Distance(m)'] = df['Distance(mm)'] / 1000.0
+df['Time'] = datetime.now() - timedelta(33) + (df.index * timedelta(hours=1))
 
-app = Dash(__name__)
-app.layout = html.Div([
-    html.H1('Bridge-mounted Water Analysis Module Data'),
-    dcc.Dropdown(id='data-dropdown',
-                 options=list(HEADER_MAP.keys()), value='Distance'),
-    html.Div(id='main-content', children=[
-        dcc.Graph(id='main-plot', style={'height': '90vh'})
-    ]),
-    dcc.Interval(
-        id='interval-component',
-        interval=3600e3,
-        n_intervals=0
-    )
-])
+app = Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
 
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink('Source Code', href='https://github.com/r3ym3l/Bridge-Mounted-Water-Analysis')),
+    ],
+    brand='Bridge-mounted Water Analysis Module Data',
+    brand_href='#',
+    color='primary',
+    dark=True,
+    style={'margin-bottom': '2rem'}
+)
+
+app.layout = dbc.Container(
+    [
+        navbar,
+        dcc.Dropdown(id='data-dropdown',
+                     options=list(HEADER_MAP.keys()), value='Distance'),
+        html.Div(id='main-content', children=[
+            dcc.Graph(id='main-plot', style={'height': '80vh'})
+        ]),
+        dcc.Interval(
+            id='interval-component',
+            interval=3600e3,
+            n_intervals=0
+        )
+    ],
+    fluid=True
+)
 
 # @app.callback(Output('main-content', 'children'),
 #               Input('interval-component', 'n_intervals'))
@@ -81,20 +109,17 @@ app.layout = html.Div([
 def change_data(option):
     if option == 'Spectral Data':
         spectral_df = df[SPECTRAL_HEADERS]
-        figure = px.line(spectral_df, x=spectral_df.index, y=spectral_df.columns)
+        figure = px.line(spectral_df, x=spectral_df.index, y=spectral_df.columns,
+                         title='Water Turbidity')
     else:
-        figure = {
-            'data': [
-                {
-                    'x': df.index,
-                    'y': df[HEADER_MAP[option]],
-                    'type': 'lines'
-                }
-            ],
-            'layout': {
-                'title': HEADER_MAP[option]
-            }
-        }
+        figure = px.line(
+           df, x='Time', y=HEADER_MAP[option],
+           title=option + ' Data'
+        )
+
+    figure.update_layout(font={
+        'size': 16
+    })
     return figure
 
 app.run_server(debug=True)
